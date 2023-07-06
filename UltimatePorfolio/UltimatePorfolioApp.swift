c4ba74ad00a85822b70ad4f1559d84fbe9827cdb
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-06 13:15:27 +0200

    Initial Commit

---
+++ UltimatePorfolio/UltimatePorfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePorfolioApp.swift
+//  UltimatePorfolio
+//
+//  Created by Eric on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePorfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
