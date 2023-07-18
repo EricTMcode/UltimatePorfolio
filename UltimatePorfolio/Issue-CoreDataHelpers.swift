bfb1497e58c99e2267347fb23561e7223c714242
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-18 13:45:22 +0200

    - create extension on Issue

---
+++ UltimatePorfolio/Issue-CoreDataHelpers.swift
@@ -0,0 +1,28 @@
+//
+//  Issue-CoreDataHelpers.swift
+//  UltimatePorfolio
+//
+//  Created by Eric on 18/07/2023.
+//
+
+import Foundation
+
+extension Issue {
+    var issueTitle: String {
+        get { title ?? "" }
+        set { title = newValue }
+    }
+    
+    var issueContent: String {
+        get { content ?? "" }
+        set { content = newValue }
+    }
+    
+    var issueCreationDate: Date {
+        creationDate ?? .now
+    }
+    
+    var issueModificationDate: Date {
+        modificationDate ?? .now
+    }
+}
